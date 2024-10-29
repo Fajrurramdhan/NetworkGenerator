ee692b05ffc5e00d6eb69fcc5e608988b41e63b2
Fajrurramdhan <fajrurrramadhan@gmail.com>
2024-10-29 16:18:52 +0700

    adding new feature side bar and layout

--- tes.py
+++ tes.py
@@ -1,8 +1,32 @@
 import streamlit as st
+import time
 
+"""# Using object notation
+add_selectbox = st.sidebar.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone")
+)"""
 
-# Set page title and layout
-st.set_page_config(page_title="Integrate Module Network Generation", layout="wide")
+# Using "with" notation
+with st.sidebar:
+    st.button("Dashboard", type="primary")
+    if st.button("Disaster"):
+        st.write("Why hello there")
+    elif st.button("Master Data"):
+         st.button("Depot")
+         st.button("Shelter")
+         st.button("Zones")
+    elif st.button("Network Generator"):
+        st.write("Why hello there")
+    elif st.button("Evacuation Planning"):
+        st.write("Why hello there")
+    elif st.button("Personal Scheduling"):
+        st.write("Why hello there")
+    elif st.button("Setting"):
+        st.write("Why hello there")
+    elif st.button("Log Out"):
+        st.write("Why hello there")
+    
 
 
 # Title
